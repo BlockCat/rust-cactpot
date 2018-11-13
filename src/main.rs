9eba455b9ba82d8401e02fdebd4c895cd47b066a
BlockCat <zinoonomiwo@gmail.com>
2018-11-13 20:40:54 +0100

    small refactor of the grid

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,65 @@
-type Grid = [[Option<u8>; 3]; 3];
-fn main() {
-    let mut grid: Grid = [[None; 3]; 3];    
+//type Grid = [[Option<u8>; 3]; 3];
+
+#[derive(Clone)]
+struct Grid {
+    grid: [[Option<u8>; 3]; 3],
+    used_digits: [bool; 9],    
+}
+
+impl Grid {
+    fn new() -> Grid {
+        Grid {
+            grid: [[None; 3]; 3],
+            used_digits: [false; 9]
+       }
+    }
+
+    fn set(&self, number: Option<u8>, point: (i32, i32)) -> Grid {
+        let mut c = self.clone();
+        let (y, x) = point;
+
+        match number {
+            Some(e) => {
+                // 
+                if let Some(cn) = c.grid[y as usize][x as usize] {
+                    // Replace number                    
+                    c.used_digits[(cn - 1) as usize] = false;
+                }                
+                c.used_digits[(e - 1) as usize] = true;
+                
+            },
+            _ => {
+                if let Some(cn) = c.grid[y as usize][x as usize] {
+                    // Replace number                    
+                    c.used_digits[(cn - 1) as usize] = false;
+                }
+            }
+        }
+        c.grid[y as usize][x as usize] = number;
+        c
+    }
+
+    fn pretty_print(&self) {
+    for i in 0..3 {
+            let a = self.grid[i][0].map(|s| s.to_string()).unwrap_or(String::from(" "));
+            let b = self.grid[i][1].map(|s| s.to_string()).unwrap_or(String::from(" "));
+            let c = self.grid[i][2].map(|s| s.to_string()).unwrap_or(String::from(" "));
+
+            println!("|{}|{}|{}|", a, b, c);
+        }
+    }
+}
+
+fn main() {    
     let payouts: [i32; 25] = [0, 0, 0, 0, 0, 0, 10_000, 36, 720, 360, 80, 252, 108, 72, 54, 180, 72, 180, 119, 36, 306, 1080, 144, 1800, 3600];
+    let grid = Grid::new().set(Some(1), (0, 0));
+    grid.pretty_print();
 
-    grid[0][0] = Some(1);
-    grid[0][1] = Some(2);   
-    grid[1][1] = Some(3);
+    println!("Score: {}", score(&grid, payouts));
     
+    println!("Next score (0, 1): {}", calculate_average_increase(&grid, payouts, (0, 1)));
+    println!("Next score (0, 2): {}", calculate_average_increase(&grid, payouts, (0, 2)));
 
-    print_grid(&grid);
-    calculate_row_candidates(&grid, 0, payouts);
-    calculate_row_candidates(&grid, 1, payouts);
-    calculate_row_candidates(&grid, 2, payouts);
-    calculate_column_candidates(&grid, 0, payouts);
-    calculate_column_candidates(&grid, 1, payouts);
-    calculate_column_candidates(&grid, 2, payouts);
 }
 
 // Numbers in a range.
@@ -22,27 +67,37 @@ fn main() {
 //
 // Calculate the expected of a row
 fn calculate_row_candidates(grid: &Grid, row: u32, payouts:[i32; 25]) -> i32 {
-   let digits = [grid[row as usize][0], grid[row as usize][1], grid[row as usize][2]];
+   let digits = [grid.grid[row as usize][0], grid.grid[row as usize][1], grid.grid[row as usize][2]];
    return calculate_expected_value(grid, payouts, digits);
 }
 
 fn calculate_column_candidates(grid: &Grid, column: u32, payouts:[i32; 25]) -> i32 {
-    let digits = [grid[0][column as usize], grid[1][column as usize], grid[0][column as usize]];
+    let digits = [grid.grid[0][column as usize], grid.grid[1][column as usize], grid.grid[2][column as usize]];
     return calculate_expected_value(grid, payouts, digits);
 }
 
+fn calculate_diagonal_candidates(grid: &Grid, diagonal: u32, payouts: [i32; 25]) -> i32 {
+    match diagonal {
+        0 => calculate_expected_value(grid, payouts, [grid.grid[0][0], grid.grid[1][1], grid.grid[2][2]]),
+        1 => calculate_expected_value(grid, payouts, [grid.grid[0][2], grid.grid[1][1], grid.grid[2][0]]),
+        _ => panic!("")
+    }
+}
+
 fn calculate_expected_value(grid: &Grid,payouts:[i32; 25], digits: [Option<u8>; 3]) -> i32 {
-    let row_digits: Vec<u8> = digits.iter().filter(|x| x.is_some()).map(|x| x.unwrap()).collect();
-    let row_sum: u8 = row_digits.iter().sum();
-    let used_digits: Vec<u8> = grid.iter().flat_map(|t| t.iter()).filter(|x| x.is_some()).map(|x| x.unwrap()).collect();
-    let unused_digits: Vec<u8> = vec![1,2,3,4,5,6,7,8,9].into_iter().filter(|x| !used_digits.contains(&x)).collect();    
-   
+    // Calculate the digits that are used in the current line
+    let row_digits: Vec<u8> = digits.iter().filter(|x| x.is_some()).map(|x| x.unwrap()).collect();    
+
+    // All digits that are used in the grid    
+    let unused_digits: Vec<u8> = grid.used_digits.iter().enumerate().filter(|(_, b)| !**b).map(|(i, _)| (i+1) as u8).collect();
+    
+    println!("unused: {:?}", unused_digits);
     // A generator that generates all possible values.
     let possible_sums: Vec<i32> = calculate_candidate_sums(row_digits.clone(), unused_digits.clone());
 
     let max_size = possible_sums.iter().map(|x| payouts[*x as usize]).sum::<i32>();
     let size: i32 = possible_sums.len() as i32;
-    println!("Current Expected value: {}", max_size / size);
+    //println!("Current Expected value: {}", max_size / size);
     return max_size / size;
 }
 
@@ -58,13 +113,31 @@ fn calculate_candidate_sums(used: Vec<u8>, unused_digits: Vec<u8>) -> Vec<i32> {
     }
 }
 
+fn calculate_average_increase(grid: &Grid, payouts: [i32; 25], point: (i32, i32)) -> i32 {
+    let (y, x) = point;
 
-fn print_grid(grid: &[[Option<u8>; 3]; 3]) {
-    for i in 0..3 {
-        let a = grid[i][0].map(|s| s.to_string()).unwrap_or(String::from(" "));
-        let b = grid[i][1].map(|s| s.to_string()).unwrap_or(String::from(" "));
-        let c = grid[i][2].map(|s| s.to_string()).unwrap_or(String::from(" "));
+    grid.used_digits.iter().enumerate().filter(|(_, b)| !**b).map(|(i, _)| {
+        score(&grid.set(Some((i + 1) as u8), (y, x)), payouts)
+    }).sum::<i32>()
+}
 
-        println!("|{}|{}|{}|", a, b, c);
+fn score(grid: &Grid, payouts: [i32; 25]) -> i32 {
+    [calculate_row_candidates(&grid, 0, payouts),
+        calculate_row_candidates(&grid, 1, payouts),
+        calculate_row_candidates(&grid, 2, payouts),        
+        calculate_column_candidates(&grid, 0, payouts),
+        calculate_column_candidates(&grid, 1, payouts),
+        calculate_column_candidates(&grid, 2, payouts),       
+        calculate_diagonal_candidates(&grid, 0, payouts),
+        calculate_diagonal_candidates(&grid, 1, payouts)].iter().sum::<i32>()      
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test] fn test_sums() {
+        let payouts: [i32; 25] = [0, 0, 0, 0, 0, 0, 10_000, 36, 720, 360, 80, 252, 108, 72, 54, 180, 72, 180, 119, 36, 306, 1080, 144, 1800, 3600];
+        let grid = Grid::new().set(Some(1), (0, 0));
+        assert_eq!(2964, score(&grid, payouts), "Testing grid");
     }
 }
